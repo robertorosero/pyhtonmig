4493a0a3cb039862750011541513e5000c120548
guido <guido@mycompany.com>
1994-01-03 20:17:07 +0000

    Include config.h if HAVE_CONFIG_H defined
    git-svn-id: http://svn.python.org/projects/python/branches/alpha100@3707 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Python/thread.c
+++ Python/thread.c
@@ -22,6 +22,10 @@ OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 ******************************************************************/
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "thread.h"
 
 #ifdef DEBUG
