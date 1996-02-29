88efd6443c9fd60cd130ad60e6100220c59e44d9
no_author <no_author@yourdomain.com>
1996-02-29 16:11:32 +0000

    This commit was manufactured by cvs2svn to create tag 'chameleon-1'.
    git-svn-id: http://svn.python.org/projects/python/tags/chameleon-1@5894 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Python/getcopyright.c
+++
@@ -1,9 +0,0 @@
-/* Return the copyright string.  This is updated manually. */
-
-#include "Python.h"
-
-const char *
-getcopyright()
-{
-	return "Copyright 1991-1995 Stichting Mathematisch Centrum, Amsterdam";
-}
--- Python/getplatform.c
+++
@@ -1,9 +0,0 @@
-#ifndef PLATFORM
-#define PLATFORM "unknown"
-#endif
-
-char *
-getplatform()
-{
-	return PLATFORM;
-}
