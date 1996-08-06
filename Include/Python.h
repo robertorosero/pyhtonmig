2aeb59ae0a14f25a53e81a56a19035ea8a7ba0a8
no_author <no_author@yourdomain.com>
1996-08-06 15:57:58 +0000

    This commit was manufactured by cvs2svn to create tag 'Beta_09-Aug-1996'.
    git-svn-id: http://svn.python.org/projects/python/tags/Beta_09-Aug-1996@6412 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Include/Python.h
+++
@@ -1,3 +0,0 @@
-/* Header file to be included by modules using new naming conventions */
-#define Py_USE_NEW_NAMES
-#include "allobjects.h"
--- Mac/Modules/ctl/ctledit.py
+++
@@ -1,8 +0,0 @@
-as_resource_body = """
-return ResObj_New((Handle)_self->ob_itself);
-"""
-
-f = ManualGenerator("as_Resource", as_resource_body)
-f.docstring = lambda : "Return this Control as a Resource"
-
-methods.append(f)
