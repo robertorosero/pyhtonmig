8330426e389736f80ca28120482ed1c80b1a67c4
no_author <no_author@yourdomain.com>
1999-04-13 15:52:45 +0000

    This commit was manufactured by cvs2svn to create tag 'r152'.
    git-svn-id: http://svn.python.org/projects/python/tags/r152@13097 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Mac/mwerks/mwerks_tkplugin_config.h
+++
@@ -1,7 +0,0 @@
-/*
-** Special config-file for _tkinter plugin.
-*/
-
-#define USE_GUSI		/* Stdio implemented with GUSI */
-#define USE_TK			/* Include _tkinter module in core Python */
-#define MAC_TCL			/* This *must* be on if USE_TK is on */
