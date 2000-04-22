e93347d688a9435166f11c7ebc7f48044a18508b
no_author <no_author@yourdomain.com>
2000-04-22 03:20:49 +0000

    This commit was manufactured by cvs2svn to create tag 'Distutils-0_8_1'.
    git-svn-id: http://svn.python.org/projects/python/tags/Distutils-0_8_1@15210 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/test/test_winsound.py
+++
@@ -1,7 +0,0 @@
-# Rediculously simple test of the winsound module for Windows.
-
-import winsound
-for i in range(100, 2000, 100):
-    winsound.Beep(i, 75)
-print "Hopefully you heard some sounds increasing in frequency!"
-
