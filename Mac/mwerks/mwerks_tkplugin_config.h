89b81437bfaf0d087bcbf3ea21246bfc8469e11b
no_author <no_author@yourdomain.com>
2000-05-03 23:44:39 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-2.108.2'.
    git-svn-id: http://svn.python.org/projects/python/branches/unlabeled-2.108.2@15337 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Mac/mwerks/mwerks_tkplugin_config.h
+++
@@ -1,9 +0,0 @@
-/*
-** Special config-file for _tkinter plugin.
-*/
-
-#define USE_GUSI1		/* Stdio implemented with GUSI */
-/* #define USE_GUSI2		/* Stdio implemented with GUSI */
-#define WITH_THREAD		/* Use thread support (needs GUSI 2, not GUSI 1) */
-#define USE_TK			/* Include _tkinter module in core Python */
-#define MAC_TCL			/* This *must* be on if USE_TK is on */
