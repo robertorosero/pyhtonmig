7bf2df1f5e47102ed0b29c61836da08b6d2bffef
gvanrossum <gvanrossum@mycompany.com>
2000-09-03 03:31:52 +0000

    Change the copyright notice according to CNRI's wishes.
    git-svn-id: http://svn.python.org/projects/python/branches/cnri-16-start@17212 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Python/getcopyright.c
+++ Python/getcopyright.c
@@ -3,8 +3,10 @@
 #include "Python.h"
 
 static char cprt[] = 
-"Copyright (c) Corporation for National Research Initiatives.\n\
-Copyright 1991-1995 Stichting Mathematisch Centrum, Amsterdam.";
+"Copyright (c) 1995-2000 Corporation for National Research Initiatives;\n\
+All Rights Reserved.\n\
+Copyright (c) 1991-1995 Stichting Mathematisch Centrum, Amsterdam;\n\
+All Rights Reserved.";
 
 const char *
 Py_GetCopyright()
