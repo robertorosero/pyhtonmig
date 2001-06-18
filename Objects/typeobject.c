c5914f3771ce73b23fdb3b9b78c05dcd4e6048a9
gvanrossum <gvanrossum@mycompany.com>
2001-06-18 13:38:54 +0000

    Add generic getattr to base object type, so object().__class__ works.
    git-svn-id: http://svn.python.org/projects/python/branches/descr-branch@21170 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -642,7 +642,7 @@ PyTypeObject PyBaseObject_Type = {
 	0,					/* tp_hash */
 	0,					/* tp_call */
 	0,					/* tp_str */
-	0,					/* tp_getattro */
+	PyObject_GenericGetAttr,		/* tp_getattro */
 	0,					/* tp_setattro */
 	0,					/* tp_as_buffer */
 	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
