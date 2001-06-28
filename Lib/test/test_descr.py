949c49389735fb6b0e2fef2e0ac908b8a2426fab
gvanrossum <gvanrossum@mycompany.com>
2001-06-28 19:52:43 +0000

    Add an __init__() method to the Number class in complexes(), now that constructors work.
    git-svn-id: http://svn.python.org/projects/python/branches/descr-branch@21382 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/test/test_descr.py
+++ Lib/test/test_descr.py
@@ -201,17 +201,19 @@ def complexes():
     numops(100.0j, 3.0j, skip=['lt', 'le', 'gt', 'ge'])
     class Number(complex):
         __slots__ = ['prec']
+        def __init__(self, *args, **kwds):
+            self.prec = kwds.get('prec', 12)
         def __repr__(self):
-            prec = self.prec or 12
+            prec = self.prec
             if self.imag == 0.0:
                 return "%.*g" % (prec, self.real)
             if self.real == 0.0:
                 return "%.*gj" % (prec, self.imag)
             return "(%.*g+%.*gj)" % (prec, self.real, prec, self.imag)
         __str__ = __repr__
-    a = Number(3.14)
-    a.prec = 12
+    a = Number(3.14, prec=6)
     verify(`a` == "3.14")
+    verify(a.prec == 6)
 
 def spamlists():
     if verbose: print "Testing spamlist operations..."
