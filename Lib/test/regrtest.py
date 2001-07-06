fc4ec16b0bbe2e4cca7bbbeb8459a5a9cf50e5c9
tim_one <tim_one@mycompany.com>
2001-07-06 21:13:05 +0000

    A change that shouldn't <wink> have been tricky.
    git-svn-id: http://svn.python.org/projects/python/branches/descr-branch@21533 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/test/regrtest.py
+++ Lib/test/regrtest.py
@@ -37,6 +37,7 @@ import os
 import getopt
 import traceback
 import random
+import StringIO
 
 import test_support
 
@@ -229,7 +230,7 @@ def runtest(test, generate, verbose, quiet, testdir = None):
     outputfile = os.path.join(outputdir, test)
     try:
         if generate:
-            cfp = open(outputfile, "w")
+            cfp = StringIO.StringIO()
         elif verbose:
             cfp = sys.stdout
         else:
@@ -243,7 +244,14 @@ def runtest(test, generate, verbose, quiet, testdir = None):
             if cfp:
                 sys.stdout = cfp
                 print test              # Output file starts with test name
-            __import__(test, globals(), locals(), [])
+            the_module = __import__(test, globals(), locals(), [])
+            # Most tests run to completion simply as a side-effect of
+            # being imported.  For the benefit of tests that can't run
+            # that way (like test_threaded_import), explicitly invoke
+            # their test_main() function (if it exists).
+            indirect_test = getattr(the_module, "test_main", None)
+            if indirect_test is not None:
+                indirect_test()
             if cfp and not (generate or verbose):
                 cfp.close()
         finally:
@@ -265,6 +273,24 @@ def runtest(test, generate, verbose, quiet, testdir = None):
             traceback.print_exc(file=sys.stdout)
         return 0
     else:
+        if generate:
+            output = cfp.getvalue()
+            if output == test + "\n":
+                if os.path.exists(outputfile):
+                    # Write it since it already exists (and the contents
+                    # may have changed), but let the user know it isn't
+                    # needed:
+                    fp = open(outputfile, "w")
+                    fp.write(output)
+                    fp.close()
+                    print "output file", outputfile, \
+                          "is no longer needed; consider removing it"
+                # else:
+                #     We don't need it, so don't create it.
+            else:
+                fp = open(outputfile, "w")
+                fp.write(output)
+                fp.close()
         return 1
 
 def findtestdir():
@@ -284,7 +310,11 @@ def count(n, word):
 class Compare:
 
     def __init__(self, filename):
-        self.fp = open(filename, 'r')
+        if os.path.exists(filename):
+            self.fp = open(filename, 'r')
+        else:
+            self.fp = StringIO.StringIO(
+                os.path.basename(filename) + "\n")
         self.stuffthatmatched = []
 
     def write(self, data):
