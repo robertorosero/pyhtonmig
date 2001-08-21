176fc4d5e355fe52f0cfd6dd3560ebe2804ec30d
gvanrossum <gvanrossum@mycompany.com>
2001-08-21 03:39:08 +0000

    Merge trunk->branch
    git-svn-id: http://svn.python.org/projects/python/branches/r22a2-branch@22659 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -33,6 +33,8 @@ class TemporaryFileTests(unittest.TestCase):
     def test_tempnam(self):
         if not hasattr(os, "tempnam"):
             return
+        warnings.filterwarnings("ignore", "tempnam", RuntimeWarning,
+                                "test_os")
         self.check_tempfile(os.tempnam())
 
         name = os.tempnam(TESTFN)
@@ -55,6 +57,8 @@ class TemporaryFileTests(unittest.TestCase):
     def test_tmpnam(self):
         if not hasattr(os, "tmpnam"):
             return
+        warnings.filterwarnings("ignore", "tmpnam", RuntimeWarning,
+                                "test_os")
         self.check_tempfile(os.tmpnam())
 
 
