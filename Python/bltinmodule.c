88aba3482b2b35e7f26d455bceee3f059bd4b4d1
jhylton <jhylton@mycompany.com>
2002-08-23 18:22:11 +0000

    Call PyRun_StringFlags() instead of the macro.
    git-svn-id: http://svn.python.org/projects/python/branches/ast-branch@28418 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Python/bltinmodule.c
+++ Python/bltinmodule.c
@@ -983,7 +983,7 @@ builtin_input(PyObject *self, PyObject *args)
 					 PyEval_GetBuiltins()) != 0)
 			return NULL;
 	}
-	res = PyRun_String(str, Py_eval_input, globals, locals);
+	res = PyRun_StringFlags(str, Py_eval_input, globals, locals, NULL);
 	Py_DECREF(line);
 	return res;
 }
