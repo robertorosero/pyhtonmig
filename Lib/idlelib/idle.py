9b7d7f521208b95cc807523fde193f070f25fe29
no_author <no_author@yourdomain.com>
2002-12-31 18:34:54 +0000

    This commit was manufactured by cvs2svn to create tag 'r23a1-fork'.
    git-svn-id: http://svn.python.org/projects/python/tags/r23a1-fork@30466 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/idlelib/idle.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-try:
-    import idlelib.PyShell
-    idlelib.PyShell.main()
-except:
-    # IDLE is not installed, but maybe PyShell is on sys.path:
-    import PyShell
-    PyShell.main()
