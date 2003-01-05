4665f86234e473751dc44eca84e69bb68395f694
jackjansen <jackjansen@mycompany.com>
2003-01-05 23:07:39 +0000

    Skip exception-propagation test if encoding isn't ascii.
    git-svn-id: http://svn.python.org/projects/python/branches/r23a1-branch@30608 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/test/test_unicode.py
+++ Lib/test/test_unicode.py
@@ -434,12 +434,13 @@ vereq((u'ab' in 'abc'), True)
 vereq((u'ab' in (1,None,u'ab')), True)
 vereq((u'' in u'abc'), True)
 vereq(('' in u'abc'), True)
-try:
-    u'\xe2' in 'g\xe2teau'
-except UnicodeError:
-    pass
-else:
-    print '*** contains operator does not propagate UnicodeErrors'
+if sys.getdefaultencoding() == 'ascii':
+    try:
+        u'\xe2' in 'g\xe2teau'
+    except UnicodeError:
+        pass
+    else:
+        print '*** contains operator does not propagate UnicodeErrors'
 print 'done.'
 
 # Formatting:
