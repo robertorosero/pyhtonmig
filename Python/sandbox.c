c26dfebaf25df07f64ee84d0af4f256474dde98f
brett.cannon <brett.cannon@mycompany.com>
2006-07-10 21:53:12 +0000

    Add Python/sandbox.c .
    git-svn-id: http://svn.python.org/projects/python/branches/bcannon-sandboxing@50549 6015fed2-1504-0410-9fe1-9d1591cc4771

---
+++ Python/sandbox.c
@@ -0,0 +1,13 @@
+#include "Python.h" /* Must be defined before PySandbox_SUPPORTED check */
+
+#ifdef PySandbox_SUPPORTED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PySandbox_SUPPORTED */
