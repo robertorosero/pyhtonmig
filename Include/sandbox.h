eca4722101107ddf45ee619e0dc48e20e709628c
brett.cannon <brett.cannon@mycompany.com>
2006-07-13 17:31:41 +0000

    Skeleton of Sandbox type.
    git-svn-id: http://svn.python.org/projects/python/branches/bcannon-sandboxing@50621 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Include/sandbox.h
+++ Include/sandbox.h
@@ -2,6 +2,7 @@
 
 #ifndef Py_SANDBOX_H
 #define Py_SANDBOX_H
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -15,6 +16,12 @@ typedef struct _sandbox_state {
 
 } PySandboxState;
 
+typedef struct {
+    PyObject_HEAD
+    PyThreadState *tstate;
+} PySandboxObject;
+
+PyAPI_DATA(PyTypeObject) PySandbox_Type;
 
 /* Return the sandbox state struct. */
 #define _PySandbox_GET() (PyThreadState_GET()->interp->sandbox_state)
--- Modules/sandboxmodule.c
+++ Modules/sandboxmodule.c
@@ -4,14 +4,71 @@
 extern "C" {
 #endif
 
+PyDoc_STRVAR(sandbox_type_doc,
+"XXX\n\
+\n\
+XXX");
+
+PyTypeObject PySandbox_Type = {
+	PyObject_HEAD_INIT(&PyType_Type)
+	0,					/* ob_size */
+	"sandbox.Sandbox",			/* tp_name */
+	sizeof(PySandboxObject),		/* tp_basicsize */
+	0,               			/* tp_itemsize */
+	0,                      		/* tp_dealloc */
+	0,					/* tp_print */
+	0,			 		/* tp_getattr */
+	0,					/* tp_setattr */
+	0,       				/* tp_compare */
+	0,               			/* tp_repr */
+	0,					/* tp_as_number */
+	0,					/* tp_as_sequence */
+	0,					/* tp_as_mapping */
+	0,                      		/* tp_hash */
+	0,              			/* tp_call */
+	0,					/* tp_str */
+	0,                      		/* tp_getattro */
+	0,	                        	/* tp_setattro */
+	0,					/* tp_as_buffer */
+	Py_TPFLAGS_DEFAULT,     		/* tp_flags */
+	sandbox_type_doc,    			/* tp_doc */
+	0,                      		/* tp_traverse */
+	0,              			/* tp_clear */
+	0,					/* tp_richcompare */
+	0,                              	/* tp_weaklistoffset */
+	0,					/* tp_iter */
+	0,					/* tp_iternext */
+	0,      				/* tp_methods */
+	0,	         			/* tp_members */
+	0,		          		/* tp_getset */
+	0,					/* tp_base */
+	0,					/* tp_dict */
+	0,					/* tp_descr_get */
+	0,					/* tp_descr_set */
+	0,                              	/* tp_dictoffset */
+	0,					/* tp_init */
+	0,					/* tp_alloc */
+	0,      				/* tp_new */
+	0,                      		/* tp_free */
+	0,              			/* tp_is_gc */
+};
+
+
 PyMODINIT_FUNC
 initsandbox(void)
 {
     PyObject *module;
 
     module = Py_InitModule3("sandbox", NULL,
-	    "Provide a sandbox to safely execute Python code.");
+	    "Provide a sandbox to safely execute Python code in.");
     if (module == NULL)
+	return;
+
+    PySandbox_Type.tp_new = PyType_GenericNew;
+    if (PyType_Ready(&PySandbox_Type) < 0)
+	return;
+
+    if (PyModule_AddObject(module, "Sandbox", (PyObject *)&PySandbox_Type) < 0)
 	return;
 }
 
