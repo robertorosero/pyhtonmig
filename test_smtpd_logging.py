cda1a6cf79db00ccf323198ca30bb14f4231a75b
jackilyn.hoxworth <jackilyn.hoxworth@mycompany.com>
2006-08-28 00:22:37 +0000

    git-svn-id: http://svn.python.org/projects/python/branches/hoxworth-stdlib_logging-soc@51629 6015fed2-1504-0410-9fe1-9d1591cc4771

---
+++ test_smtpd_logging.py
@@ -0,0 +1,23 @@
+import smtpd
+import logging
+from cStringIO import StringIO
+
+# ... run the tests ...
+log=logging.getLogger("py.smtpd")
+stringLog = StringIO()
+
+# define the handler and level
+handler = logging.StreamHandler(stringLog)
+log.setLevel(logging.INFO)
+
+# add the handler to the logger
+log.addHandler(handler)
+
+smtpd._log.info("message 1") 
+
+print stringLog.getvalue()  # For testing purposes
+
+if stringLog.getvalue() != "Error:  It worked":
+    print "it worked"
+else:
+    print "it didn't work"
