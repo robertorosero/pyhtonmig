67d828b220502f991c96714102a9f41b3fc19b51
brett.cannon <brett.cannon@mycompany.com>
2007-05-25 23:54:04 +0000

    Echo info out about  what is being compiled.  Also add warnings and debugging symbols.
    git-svn-id: http://svn.python.org/projects/python/branches/bcannon-objcap@55596 6015fed2-1504-0410-9fe1-9d1591cc4771

--- build_secure_py.sh
+++ build_secure_py.sh
@@ -1,2 +1,4 @@
-gcc -c -IInclude -I. secure_python.c
+echo "Build secure_python.o ..."
+gcc -Wall -g -c -IInclude -I. secure_python.c
+echo "Build executable ..."
 gcc -o secure_python.exe secure_python.o libpython2.6.a
