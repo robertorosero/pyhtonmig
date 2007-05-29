f1b13100affd026f4bfcc7a4b4831c38c0c148a0
brett.cannon <brett.cannon@mycompany.com>
2007-05-29 20:22:34 +0000

    Change test for 'sys' on required modules for the interpreter to also check for 'open' and 'execfile'.  Rename the test accordingly.
    git-svn-id: http://svn.python.org/projects/python/branches/bcannon-objcap@55658 6015fed2-1504-0410-9fe1-9d1591cc4771

---
+++ tests/fail/dangerous_things_inaccessible.py
@@ -0,0 +1,48 @@
+"""None of the modules required for the interpreter to work should expose the
+'sys' module, 'open', or 'execfile'.  This holds for the modules that they
+import as well."""
+# Stuff needed to look for sys.
+import encodings
+module_type = type(encodings)
+examined = set()
+# Needed to look for 'open' and 'execfile'.
+builtin_fxn_type = type(any)
+dangerous_builtins = ('open', 'execfile')
+
+def check_imported_modules(module):
+    """Recursively check that the module (and the modules it imports) do not
+    expose the 'sys' module."""
+    assert isinstance(module, module_type)
+    if module.__name__ == 'sys':
+        raise Exception
+    for attr in module.__dict__.values():
+        # If an object doesn't define __name__ then we don't care about it.
+        try:
+            attr_name = attr.__name__
+        except AttributeError:
+            continue
+        if isinstance(attr, module_type) and attr.__name__ not in examined:
+            examined.add(attr_name)
+            check_imported_modules(attr)
+        elif isinstance(attr, builtin_fxn_type):
+            if attr_name in dangerous_builtins:
+                raise Exception
+
+
+import __builtin__
+check_imported_modules(__builtin__)
+
+import __main__
+check_imported_modules(__main__)
+
+import exceptions
+check_imported_modules(exceptions)
+
+import encodings
+check_imported_modules(encodings)
+
+import codecs
+check_imported_modules(codecs)
+
+import _codecs
+check_imported_modules(_codecs)
