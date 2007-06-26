e251a184529d24585a0b2b85933bac1b2fea4e19
brett.cannon <brett.cannon@mycompany.com>
2007-06-26 19:20:39 +0000

    Update ld arguments.
    git-svn-id: http://svn.python.org/projects/python/branches/bcannon-objcap@56090 6015fed2-1504-0410-9fe1-9d1591cc4771

--- build_secure_py.sh
+++ build_secure_py.sh
@@ -1,4 +1,4 @@
 echo "Build secure_python.o ..."
 gcc -Wall -g -c -IInclude -I. secure_python.c
 echo "Build executable ..."
-gcc -o secure_python.exe secure_python.o libpython2.6.a
+gcc -L. -ldl -lpython2.6 -o secure_python.exe secure_python.o
