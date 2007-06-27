0c4d0b5272000af247a3d5a6119023213aad1735
brett.cannon <brett.cannon@mycompany.com>
2007-06-27 18:37:46 +0000

    Add my linked libraries.
    git-svn-id: http://svn.python.org/projects/python/branches/bcannon-objcap@56095 6015fed2-1504-0410-9fe1-9d1591cc4771

--- build_secure_py.sh
+++ build_secure_py.sh
@@ -1,4 +1,4 @@
 echo "Build secure_python.o ..."
 gcc -Wall -g -c -IInclude -I. secure_python.c
 echo "Build executable ..."
-gcc -L. -ldl -lpython2.6 -o secure_python.exe secure_python.o
+gcc -L. -lpthread -lm -ldl -o secure_python.exe secure_python.o libpython2.6.a
