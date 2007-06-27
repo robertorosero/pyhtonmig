254b259572f1ef0a2d646a5d6c7cd0d949859ec0
brett.cannon <brett.cannon@mycompany.com>
2007-06-27 21:47:16 +0000

    Add a ton of debugging info.
    git-svn-id: http://svn.python.org/projects/python/branches/bcannon-objcap@56101 6015fed2-1504-0410-9fe1-9d1591cc4771

--- secure_python.c
+++ secure_python.c
@@ -1,11 +1,10 @@
 /*
    Proof-of-concept application that embeds Python with security features
    turned on to prevent unmitigated access to resources.
-
-   XXX See BRANCH_NOTES for what needs to be done.
 */
 #include "Python.h"
 
+/* XXX Add error checking. */
 #define CREATE_SAFE_LIST(kind)  \
 	safe_##kind##_seq = PyTuple_New(safe_##kind##_count); \
         for (x = 0; x < safe_##kind##_count; x += 1) { \
@@ -59,12 +58,26 @@ main(int argc, char *argv[])
        This needs to be done before importlib is called as it sets values in
        both attributes. */
     obj = PyDict_GetItemString(interp->sysdict, "meta_path");
+    if (PyErr_Occurred()) {
+	    fprintf(stderr, "Fetching sys.meta_path failed.\n");
+	    return 1;
+    }
     x = PyList_Size(obj);
-    PyList_SetSlice(obj, 0, x, NULL);
+    if (PyList_SetSlice(obj, 0, x, NULL) == -1) {
+	    fprintf(stderr, "Clearing sys.meta_path failed.\n");
+	    return 1;
+    }
 
     obj = PyDict_GetItemString(interp->sysdict, "path_hooks");
+    if (PyErr_Occurred()) {
+	    fprintf(stderr, "Fetching sys.path_hooks failed.\n");
+	    return 1;
+    }
     x = PyList_Size(obj);
-    PyList_SetSlice(obj, 0, x, NULL);
+    if (PyList_SetSlice(obj, 0, x, NULL) == -1) {
+	    fprintf(stderr, "Clearing sys.path_hooks failed.\n");
+	    return 1;
+    }
 
     /* Create lists of modules safe to import. */
     CREATE_SAFE_LIST(builtins);
@@ -74,21 +87,33 @@ main(int argc, char *argv[])
 
     /* Get importer from importlib. */
     import_module = PyImport_ImportModule("controlled_importlib");
-    if (!import_module)
+    if (!import_module) {
+	    fprintf(stderr, "Import of controlled_importlib failed.\n");
 	    return 1;
+    }
 
     import_callable = PyObject_CallMethod(import_module,
 		    "ControlledImport", "(O, O, O)",
 		    safe_builtins_seq, safe_frozen_seq, safe_extensions_seq);
-    if (!import_callable)
+    if (!import_callable) {
+	    fprintf(stderr, "Instantiating "
+			    "controlled_importlib.ControlledImport failed.\n");
 	    return 1;
+    }
 
     /* Store importlib importer somewhere. */
-    PyDict_SetItemString(interp->sysdict, "import_", import_callable);
+    if (PyDict_SetItemString(interp->sysdict, "import_", import_callable) ==
+		    -1) {
+	    fprintf(stderr, "Setting sys.import_ failed.\n");
+	    return 1;
+    }
     /* Set __import__ to the import delegate defined in 'sys'. */
-    PyDict_SetItemString(interp->builtins, "__import__",
+    if (PyDict_SetItemString(interp->builtins, "__import__",
 		    PyDict_GetItemString(interp->sysdict,
-			    "import_delegate"));
+			    "import_delegate")) == -1) {
+	    fprintf(stderr, "Resetting __import__ failed.\n");
+	    return 1;
+    }
 
     Py_DECREF(import_module);
     Py_DECREF(import_callable);
@@ -119,8 +144,16 @@ main(int argc, char *argv[])
     /* Get the 'warnings' module cached away at the C level. */
     PyModule_GetWarningsModule();
     module_names_list = PyDict_Keys(interp->modules);
+    if (!module_names_list) {
+	    fprintf(stderr, "sys.modules.keys() failed.\n");
+	    return 1;
+    }
     module_count = PyList_GET_SIZE(module_names_list);
     hidden_modules = PyDict_New();
+    if (!hidden_modules) {
+	    fprintf(stderr, "Creating an empty dict for .hidden failed.\n");
+	    return 1;
+    }
     for (x=0; x < module_count; x+=1) {
 	    char *module_name =
 		    PyString_AS_STRING(
@@ -138,26 +171,58 @@ main(int argc, char *argv[])
 		    PyObject *module =
 			    PyDict_GetItemString(interp->modules,
 					    module_name);
-		    PyDict_SetItemString(hidden_modules, module_name,
-				    module);
-		    PyDict_DelItemString(interp->modules, module_name);
+		    if (PyErr_Occurred()) {
+			    fprintf(stderr, "Fetching %s from sys.modules "
+					    "failed.\n", module_name);
+			    return 1;
+		    }
+		    if (PyDict_SetItemString(hidden_modules, module_name,
+				    module) == -1) {
+			    fprintf(stderr, "Adding %s to .hidden failed.\n",
+					    module_name);
+			    return 1;
+		    }
+		    if (PyDict_DelItemString(interp->modules, module_name) ==
+				    -1) {
+			    fprintf(stderr, "Removing %s from sys.modules "
+					    "failed.\n", module_name);
+			    return 1;
+		    }
 	    }
     }
     /* Store away modules that must stick around but should not be exposed;
        this is done for importlib's benefit.
        */
-    PyDict_SetItemString(interp->modules, ".hidden", hidden_modules);
+    if (PyDict_SetItemString(interp->modules, ".hidden", hidden_modules) == -1)
+    {
+	    fprintf(stderr, "Adding .hidden to sys.modules failed.\n");
+	    return 1;
+    }
 
-    PyDict_SetItemString(interp->sysdict, "modules", interp->modules);
+    //PyDict_SetItemString(interp->sysdict, "modules", interp->modules);
 
     /* Clear out sys.path_importer_cache. */
     PyDict_Clear(PyDict_GetItemString(interp->sysdict,
 			    "path_importer_cache"));
+    if (PyErr_Occurred()) {
+	    fprintf(stderr, "An occurred while clearing "
+			    "sys.path_importer_cache.\n");
+	    return 1;
+    }
 
     /* Remove dangerous built-ins. */
-    PyDict_DelItemString(interp->builtins, "execfile");
-    PyDict_DelItemString(interp->builtins, "open");
-    PyDict_DelItemString(interp->builtins, "SystemExit");
+    if (PyDict_DelItemString(interp->builtins, "execfile") == -1) {
+	    fprintf(stderr, "Removing execfile failed.\n");
+	    return 1;
+    }
+    if (PyDict_DelItemString(interp->builtins, "open") == -1) {
+	    fprintf(stderr, "Removing 'open' failed.\n");
+	    return 1;
+    }
+    if (PyDict_DelItemString(interp->builtins, "SystemExit") == -1) {
+	    fprintf(stderr, "Removing SystemExit failed.\n");
+	    return 1;
+    }
 
   /* Use interpreter. */
     return_val = Py_Main(argc, argv);
