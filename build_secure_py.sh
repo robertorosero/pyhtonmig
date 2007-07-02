98546b5a6daf6330e99a7aef459f2355ba561d6c
brett.cannon <brett.cannon@mycompany.com>
2007-07-02 03:01:47 +0000

    Add a comment about what someone might need to change to build.
    git-svn-id: http://svn.python.org/projects/python/branches/bcannon-objcap@56141 6015fed2-1504-0410-9fe1-9d1591cc4771

--- build_secure_py.sh
+++ build_secure_py.sh
@@ -1,4 +1,5 @@
 echo "Build secure_python.o ..."
 gcc -Wall -g -c -IInclude -I. secure_python.c
 echo "Build executable ..."
+# If the command below fails, might need to add -lutil or -Wl,-E .
 gcc -L. -lpthread -lm -ldl -o secure_python.exe secure_python.o libpython2.6.a
