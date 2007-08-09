de74c74e1e8dd2941182ee9707fef199f2e7fe72
guido.van.rossum <guido.van.rossum@mycompany.com>
2007-08-09 20:11:56 +0000

    Empty out this thread, leaving only the tombstone.
    git-svn-id: http://svn.python.org/projects/python/branches/py3k-struni@56871 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Tools/buildbot/build-amd64.bat
+++
@@ -1,6 +0,0 @@
-@rem Used by the buildbot "compile" step.
-setlocal
-cmd /c Tools\buildbot\external-amd64.bat
-call "%VS71COMNTOOLS%vsvars32.bat"
-REM cmd /q/c Tools\buildbot\kill_python.bat
-devenv.com /build ReleaseAMD64 PCbuild\pcbuild.sln
