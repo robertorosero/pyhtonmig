8a8ccdb7d32bc1c5e441332b9af8264212f97c1c
thomas.lee <thomas.lee@mycompany.com>
2008-06-25 13:47:24 +0000

    Merged revisions 64503-64524 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r64508 | mark.dickinson | 2008-06-25 01:35:14 +1000 (Wed, 25 Jun 2008) | 2 lines
      
      Remove references to Py3k in __floor__, __ceil__ and __round__ documentation.
    ........
      r64511 | raymond.hettinger | 2008-06-25 01:58:53 +1000 (Wed, 25 Jun 2008) | 1 line
      
      Issue 3189:  Py3k DeprecationWarning in difflib
    ........
      r64516 | raymond.hettinger | 2008-06-25 08:46:07 +1000 (Wed, 25 Jun 2008) | 1 line
      
      Revert 64424, 64438, and 64439.
    ........
      r64517 | benjamin.peterson | 2008-06-25 13:09:05 +1000 (Wed, 25 Jun 2008) | 1 line
      
      remove bytes alias in multiprocessing
    ........
      r64518 | robert.schuppenies | 2008-06-25 19:20:03 +1000 (Wed, 25 Jun 2008) | 2 lines
      
      Issue 3147: Fixed SizeofTest failure for LLP64 systems.
    ........
      r64519 | benjamin.peterson | 2008-06-25 22:39:05 +1000 (Wed, 25 Jun 2008) | 1 line
      
      use byte literals in multiprocessing
    ........
      r64520 | benjamin.peterson | 2008-06-25 22:44:29 +1000 (Wed, 25 Jun 2008) | 1 line
      
      get rid of 2.6/3.0 switch statements in multiprocessing
    ........
    
    
    git-svn-id: http://svn.python.org/projects/python/branches/tlee-ast-optimize@64525 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/multiprocessing/managers.py
+++ Lib/multiprocessing/managers.py
@@ -33,15 +33,6 @@ except ImportError:
     from pickle import PicklingError
 
 #
-#
-#
-
-try:
-    bytes
-except NameError:
-    bytes = str                  # XXX not needed in Py2.6 and Py3.0
-
-#
 # Register some things for pickling
 #
 
@@ -50,11 +41,6 @@ def reduce_array(a):
 copy_reg.pickle(array.array, reduce_array)
 
 view_types = [type(getattr({}, name)()) for name in ('items','keys','values')]
-if view_types[0] is not list:       # XXX only needed in Py3.0
-    def rebuild_as_list(obj):
-        return list, (list(obj),)
-    for view_type in view_types:
-        copy_reg.pickle(view_type, rebuild_as_list)
 
 #
 # Type for identifying shared objects
