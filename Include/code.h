robertorosero/pyhtonmig

commit 4a745c8fc9d530ffe1d3210ec674f3b3ae060b04

Issue #1717, continued:  remove PyObject_Compare and Py_CmpToRich declarations from object.h; don't inherit tp_compare slot on subclasses; and raise TypeError when initializing a type that has a nonzero tp_compare slot.  Fix up comparison-related comments in object.c and code.h.
git-svn-id: http://svn.python.org/projects/python/branches/py3k@69188 6015fed2-1504-0410-9fe1-9d1591cc4771

Include/code.h
@@ -20,7 +20,7 @@ typedef struct {
     PyObject *co_varnames;	/* tuple of strings (local variable names) */
     PyObject *co_freevars;	/* tuple of strings (free variable names) */
     PyObject *co_cellvars;      /* tuple of strings (cell variable names) */
-    /* The rest doesn't count for hash/cmp */
+    /* The rest doesn't count for hash or comparisons */
     PyObject *co_filename;	/* unicode (where it was loaded from) */
     PyObject *co_name;		/* unicode (name, for reference) */
     int co_firstlineno;		/* first source line number */

Objects/typeobject.c
@@ -3662,7 +3662,7 @@ inherit_slots(PyTypeObject *type, PyTypeObject *base)
 		type->tp_setattr = base->tp_setattr;
 		type->tp_setattro = base->tp_setattro;
 	}
-	/* tp_compare see tp_richcompare */
+	/* tp_compare is ignored, see tp_richcompare */
 	COPYSLOT(tp_repr);
 	/* tp_hash see tp_richcompare */
 	COPYSLOT(tp_call);
@@ -3670,12 +3670,10 @@ inherit_slots(PyTypeObject *type, PyTypeObject *base)
 	{
 		/* Copy comparison-related slots only when
 		   not overriding them anywhere */
-		if (type->tp_compare == NULL &&
-		    type->tp_richcompare == NULL &&
+		if (type->tp_richcompare == NULL &&
 		    type->tp_hash == NULL &&
 		    !overrides_hash(type))
 		{
-			type->tp_compare = base->tp_compare;
 			type->tp_richcompare = base->tp_richcompare;
 			type->tp_hash = base->tp_hash;
 		}
@@ -3886,6 +3884,13 @@ PyType_Ready(PyTypeObject *type)
 		if (PyType_Check(b) &&
 		    add_subclass((PyTypeObject *)b, type) < 0)
 			goto error;
+	}
+
+	/* Check reserved slots */
+	if (type->tp_compare) {
+		PyErr_Format(PyExc_TypeError,
+			     "type %s has tp_compare",
+			     type->tp_name);
 	}
 
 	/* All done -- set the ready flag */

Include/object.h
@@ -426,10 +426,8 @@ PyAPI_FUNC(PyObject *) PyObject_Repr(PyObject *);
 PyAPI_FUNC(PyObject *) PyObject_Str(PyObject *);
 PyAPI_FUNC(PyObject *) PyObject_ASCII(PyObject *);
 PyAPI_FUNC(PyObject *) PyObject_Bytes(PyObject *);
-PyAPI_FUNC(int) PyObject_Compare(PyObject *, PyObject *);
 PyAPI_FUNC(PyObject *) PyObject_RichCompare(PyObject *, PyObject *, int);
 PyAPI_FUNC(int) PyObject_RichCompareBool(PyObject *, PyObject *, int);
-PyAPI_FUNC(PyObject *) Py_CmpToRich(int op, int cmp);
 PyAPI_FUNC(PyObject *) PyObject_GetAttrString(PyObject *, const char *);
 PyAPI_FUNC(int) PyObject_SetAttrString(PyObject *, const char *, PyObject *);
 PyAPI_FUNC(int) PyObject_HasAttrString(PyObject *, const char *);

Objects/object.c
@@ -500,35 +500,19 @@ PyObject_Bytes(PyObject *v)
 	return PyBytes_FromObject(v);
 }
 
-/* The new comparison philosophy is: we completely separate three-way
-   comparison from rich comparison.  That is, PyObject_Compare() and
-   PyObject_Cmp() *just* use the tp_compare slot.  And PyObject_RichCompare()
-   and PyObject_RichCompareBool() *just* use the tp_richcompare slot.
+/* For Python 3.0.1 and later, the old three-way comparison has been
+   completely removed in favour of rich comparisons.  PyObject_Compare() and
+   PyObject_Cmp() are gone, and the builtin cmp function no longer exists.
+   The old tp_compare slot will be renamed to tp_reserved, and should no
+   longer be used.  Use tp_richcompare instead.
 
    See (*) below for practical amendments.
 
-   IOW, only cmp() uses tp_compare; the comparison operators (==, !=, <=, <,
-   >=, >) only use tp_richcompare.  Note that list.sort() only uses <.
+   tp_richcompare gets called with a first argument of the appropriate type
+   and a second object of an arbitrary type.  We never do any kind of
+   coercion.
 
-   (And yes, eventually we'll rip out cmp() and tp_compare.)
-
-   The calling conventions are different: tp_compare only gets called with two
-   objects of the appropriate type; tp_richcompare gets called with a first
-   argument of the appropriate type and a second object of an arbitrary type.
-   We never do any kind of coercion.
-
-   The return conventions are also different.
-
-   The tp_compare slot should return a C int, as follows:
-
-     -1 if a < b or if an exception occurred
-      0 if a == b
-     +1 if a > b
-
-   No other return values are allowed.  PyObject_Compare() has the same
-   calling convention.
-
-  The tp_richcompare slot should return an object, as follows:
+   The tp_richcompare slot should return an object, as follows:
 
     NULL if an exception occurred
     NotImplemented if the requested comparison is not implemented
@@ -543,9 +527,6 @@ PyObject_Bytes(PyObject *v)
   - If rich comparison returns NotImplemented, == and != are decided by
     comparing the object pointer (i.e. falling back to the base object
     implementation).
-
-  - If three-way comparison is not implemented, it falls back on rich
-    comparison (but not the other way around!).
 
 */
 

commit 04fc26f36e93392d6247650274b5af047bee3963

Merged revisions 69188,69190,69192,69197-69198,69202,69207,69209-69210,69213-69214 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
................
  r69188 | mark.dickinson | 2009-02-01 13:59:22 +0000 (Sun, 01 Feb 2009) | 6 lines
  
  Issue #1717, continued:  remove PyObject_Compare and Py_CmpToRich declarations
  from object.h; don't inherit tp_compare slot on subclasses; and raise TypeError
  when initializing a type that has a nonzero tp_compare slot.  Fix up
  comparison-related comments in object.c and code.h.
................
  r69190 | mark.dickinson | 2009-02-01 14:18:10 +0000 (Sun, 01 Feb 2009) | 2 lines
  
  Issue #1717: documentation fixes related to the cmp removal.
................
  r69192 | mark.dickinson | 2009-02-01 20:36:08 +0000 (Sun, 01 Feb 2009) | 2 lines
  
  Remove exception for non-NULL tp_compare that was introduced in r69188.
................
  r69197 | guilherme.polo | 2009-02-02 01:02:20 +0000 (Mon, 02 Feb 2009) | 10 lines
  
  Recorded merge of revisions 69195 via svnmerge from 
  svn+ssh://pythondev/python/trunk
  
  ........
    r69195 | guilherme.polo | 2009-02-01 22:38:54 -0200 (Sun, 01 Feb 2009) | 3 lines
    
    Use a single Tcl interpreter through all these tests, this may help some 
    failing buildbots.
  ........
................
  r69198 | brett.cannon | 2009-02-02 05:58:30 +0000 (Mon, 02 Feb 2009) | 3 lines
  
  Note to move __package__ stuff over to rpartition to make Nick happy. Also make
  sure that accepting the empty string for top-level modules is okay.
................
  r69202 | hirokazu.yamamoto | 2009-02-02 09:15:41 +0000 (Mon, 02 Feb 2009) | 9 lines
  
  Merged revisions 69200 via svnmerge from 
  svn+ssh://pythondev@svn.python.org/python/trunk
  
  ........
    r69200 | hirokazu.yamamoto | 2009-02-02 17:01:17 +0900 | 1 line
    
    Set native svn:eol-style property for text files.
  ........
................

Include/code.h
@@ -20,7 +20,7 @@ typedef struct {
     PyObject *co_varnames;	/* tuple of strings (local variable names) */
     PyObject *co_freevars;	/* tuple of strings (free variable names) */
     PyObject *co_cellvars;      /* tuple of strings (cell variable names) */
-    /* The rest doesn't count for hash */
+    /* The rest doesn't count for hash or comparisons */
     PyObject *co_filename;	/* unicode (where it was loaded from) */
     PyObject *co_name;		/* unicode (name, for reference) */
     int co_firstlineno;		/* first source line number */

Lib/http/client.py
@@ -265,14 +265,14 @@ class HTTPResponse:
     # accepts iso-8859-1.
 
     def __init__(self, sock, debuglevel=0, strict=0, method=None):
-        # XXX If the response includes a content-length header, we
+        # If the response includes a content-length header, we
         # need to make sure that the client doesn't read more than the
         # specified number of bytes.  If it does, it will block until
         # the server times out and closes the connection.  (The only
-        # applies to HTTP/1.1 connections.)  Since some clients access
-        # self.fp directly rather than calling read(), this is a little
-        # tricky.
-        self.fp = sock.makefile("rb", 0)
+        # applies to HTTP/1.1 connections.)  This will happen if a self.fp.read()
+        # is done (without a size) whether self.fp is buffered or not.
+        # So, no self.fp.read() by clients unless they know what they are doing.
+        self.fp = sock.makefile("rb")
         self.debuglevel = debuglevel
         self.strict = strict
         self._method = method

Lib/importlib/NOTES
@@ -1,6 +1,12 @@
 to do
 /////
 
+* Create sandbox directory for a distutils packaging of what is in Python 2.7.
+
+* Use rpartition for getting the package of a module.
+
+    + Make sure that an empty string is acceptable for __package__.
+
 * Create meta_path importer for sys.path.
 
     + Create hook.
@@ -73,3 +79,9 @@ to do
     - Relative name from sys.path.
 
 * Bootstrap importlib as implementation of builtins.__import__
+
+* Replace standard library modules.
+
+  + imp
+  + py_compile
+  + compileall

Lib/importlib/_bootstrap.py
@@ -589,6 +589,82 @@ class PyFileImporter(FileImporter):
         super(PyFileImporter, self).__init__(path_entry)
 
 
+class SysPathFinder:
+
+    """Meta path finder for sys.(path|path_hooks|path_importer_cache)."""
+
+    def _default_hook(self, path):
+        """Use the default hook on 'path'.
+
+        If the path will not work for the default hook then raise ImportError.
+
+        """
+        # TODO(brett.cannon) Implement
+        raise ImportError
+
+    # The list of implicit hooks cannot be a class attribute because of
+    # bootstrapping issues for accessing imp.
+    def _implicit_hooks(self, path):
+        """Return a list of the implicit path hooks."""
+        return [self._default_hook, imp.NullImporter]
+
+    def _path_hooks(self, path):
+        """Search sys.path_hooks for a finder for 'path'.
+
+        Guaranteed to return a finder for the path as NullImporter is the
+        default importer for any path that does not have an explicit finder.
+
+        """
+        for hook in sys.path_hooks + self._implicit_hooks():
+            try:
+                return hook(path)
+            except ImportError:
+                continue
+        else:
+            # This point should never be reached thanks to NullImporter.
+            raise SystemError("no hook could find an importer for "
+                              "{0}".format(path))
+
+    def _path_importer_cache(self, path):
+        """Get the finder for the path from sys.path_importer_cache.
+
+        If the path is not in the cache, find the appropriate finder and cache
+        it. If None is cached, get the default finder and cache that
+        (if applicable).
+
+        Because of NullImporter, some finder should be returned. The only
+        explicit fail case is if None is cached but the path cannot be used for
+        the default hook, for which ImportError is raised.
+
+        """
+        try:
+            finder = sys.path_importer_cache(path);
+        except KeyError:
+            finder = self._path_hooks(path)
+            sys.path_importer_cache[path] = finder
+        else:
+            if finder is None:
+                # Raises ImportError on failure.
+                finder = self._default_hook(path)
+                sys.path_importer_cache[path] = finder
+        return finder
+
+    def find_module(self, fullname, path=None):
+        """Find the module on sys.path or 'path'."""
+        if not path:
+            path = sys.path
+        for entry in path:
+            try:
+                finder = self._path_importer_cache(entry)
+            except ImportError:
+                continue
+            loader = finder.find_module(fullname)
+            if loader:
+                return loader
+        else:
+            return None
+
+
 class ImportLockContext(object):
 
     """Context manager for the import lock."""

Lib/xmlrpc/client.py
@@ -1129,7 +1129,7 @@ class Transport:
 
         self.verbose = verbose
 
-        return self._parse_response(resp, None)
+        return self.parse_response(resp)
 
     ##
     # Create parser.
@@ -1212,29 +1212,12 @@ class Transport:
     # @return Response tuple and target method.
 
     def parse_response(self, file):
-        # compatibility interface
-        return self._parse_response(file, None)
-
-    ##
-    # Parse response (alternate interface).  This is similar to the
-    # parse_response method, but also provides direct access to the
-    # underlying socket object (where available).
-    #
-    # @param file Stream.
-    # @param sock Socket handle (or None, if the socket object
-    #    could not be accessed).
-    # @return Response tuple and target method.
-
-    def _parse_response(self, file, sock):
         # read response from input file/socket, and parse it
 
         p, u = self.getparser()
 
         while 1:
-            if sock:
-                response = sock.recv(1024)
-            else:
-                response = file.read(1024)
+            response = file.read(1024)
             if not response:
                 break
             if self.verbose:

Modules/_multiprocessing/connection.h
@@ -487,7 +487,7 @@ PyTypeObject CONNECTION_TYPE = {
 	/* tp_print          */ 0,
 	/* tp_getattr        */ 0,
 	/* tp_setattr        */ 0,
-	/* tp_reserved        */ 0,
+	/* tp_reserved       */ 0,
 	/* tp_repr           */ (reprfunc)connection_repr,
 	/* tp_as_number      */ 0,
 	/* tp_as_sequence    */ 0,

Modules/_multiprocessing/semaphore.c
@@ -592,7 +592,7 @@ PyTypeObject SemLockType = {
 	/* tp_print          */ 0,
 	/* tp_getattr        */ 0,
 	/* tp_setattr        */ 0,
-	/* tp_reserved        */ 0,
+	/* tp_reserved       */ 0,
 	/* tp_repr           */ 0,
 	/* tp_as_number      */ 0,
 	/* tp_as_sequence    */ 0,

Modules/_testcapimodule.c
@@ -187,7 +187,7 @@ static PyTypeObject _HashInheritanceTester_Type = {
 	0,			/* tp_print */
 	0,			/* tp_getattr */
 	0,			/* tp_setattr */
-	0,			/* tp_compare */
+	0,			/* tp_reserved */
 	0,			/* tp_repr */
 	0,			/* tp_as_number */
 	0,			/* tp_as_sequence */

Modules/_threadmodule.c
@@ -358,7 +358,7 @@ static PyTypeObject localtype = {
 	/* tp_print          */ 0,
 	/* tp_getattr        */ 0,
 	/* tp_setattr        */ 0,
-	/* tp_reserved        */ 0,
+	/* tp_reserved       */ 0,
 	/* tp_repr           */ 0,
 	/* tp_as_number      */ 0,
 	/* tp_as_sequence    */ 0,

Modules/itertoolsmodule.c
@@ -2273,7 +2273,7 @@ static PyTypeObject cwr_type = {
 	0,						/* tp_print */
 	0,						/* tp_getattr */
 	0,						/* tp_setattr */
-	0,						/* tp_compare */
+	0,						/* tp_reserved */
 	0,						/* tp_repr */
 	0,						/* tp_as_number */
 	0,						/* tp_as_sequence */
@@ -2700,7 +2700,7 @@ static PyTypeObject compress_type = {
 	0,								/* tp_print */
 	0,								/* tp_getattr */
 	0,								/* tp_setattr */
-	0,								/* tp_compare */
+	0,								/* tp_reserved */
 	0,								/* tp_repr */
 	0,								/* tp_as_number */
 	0,								/* tp_as_sequence */

Objects/typeobject.c
@@ -3662,7 +3662,7 @@ inherit_slots(PyTypeObject *type, PyTypeObject *base)
 		type->tp_setattr = base->tp_setattr;
 		type->tp_setattro = base->tp_setattro;
 	}
-	/* tp_reserved is ignored, see tp_richcompare */
+	/* tp_reserved is ignored */
 	COPYSLOT(tp_repr);
 	/* tp_hash see tp_richcompare */
 	COPYSLOT(tp_call);

Objects/unicodeobject.c
@@ -4566,7 +4566,7 @@ static PyTypeObject EncodingMapType = {
     0,                      /*tp_print*/
     0,                      /*tp_getattr*/
     0,                      /*tp_setattr*/
-    0,                      /*tp_compare*/
+    0,                      /*tp_reserved*/
     0,                      /*tp_repr*/
     0,                      /*tp_as_number*/
     0,                      /*tp_as_sequence*/
@@ -9493,7 +9493,7 @@ PyTypeObject PyUnicode_Type = {
     0,                  /* tp_print */
     0,                  /* tp_getattr */
     0,                  /* tp_setattr */
-    0,                  /* tp_compare */
+    0,                  /* tp_reserved */
     unicode_repr,           /* tp_repr */
     &unicode_as_number,         /* tp_as_number */
     &unicode_as_sequence,       /* tp_as_sequence */
@@ -9799,7 +9799,7 @@ PyTypeObject PyUnicodeIter_Type = {
     0,                  /* tp_print */
     0,                  /* tp_getattr */
     0,                  /* tp_setattr */
-    0,                  /* tp_compare */
+    0,                  /* tp_reserved */
     0,                  /* tp_repr */
     0,                  /* tp_as_number */
     0,                  /* tp_as_sequence */

PC/getpathp.c
@@ -200,6 +200,7 @@ search_for_prefix(wchar_t *argv0_path, wchar_t *landmark)
 }
 
 #ifdef MS_WINDOWS
+#ifdef Py_ENABLE_SHARED
 
 /* a string loaded from the DLL at startup.*/
 extern const char *PyWin_DLLVersionString;
@@ -349,6 +350,7 @@ done:
 		free(keyBuf);
 	return retval;
 }
+#endif /* Py_ENABLE_SHARED */
 #endif /* MS_WINDOWS */
 
 static void
@@ -359,11 +361,15 @@ get_progpath(void)
 	wchar_t *prog = Py_GetProgramName();
 
 #ifdef MS_WINDOWS
+#ifdef Py_ENABLE_SHARED
 	extern HANDLE PyWin_DLLhModule;
 	/* static init of progpath ensures final char remains \0 */
 	if (PyWin_DLLhModule)
 		if (!GetModuleFileNameW(PyWin_DLLhModule, dllpath, MAXPATHLEN))
 			dllpath[0] = 0;
+#else
+	dllpath[0] = 0;
+#endif
 	if (GetModuleFileNameW(NULL, progpath, MAXPATHLEN))
 		return;
 #endif
@@ -471,8 +477,10 @@ calculate_path(void)
 	}
  
 	skiphome = pythonhome==NULL ? 0 : 1;
+#ifdef Py_ENABLE_SHARED
 	machinepath = getpythonregpath(HKEY_LOCAL_MACHINE, skiphome);
 	userpath = getpythonregpath(HKEY_CURRENT_USER, skiphome);
+#endif
 	/* We only use the default relative PYTHONPATH if we havent
 	   anything better to use! */
 	skipdefault = envpath!=NULL || pythonhome!=NULL || \

Parser/asdl_c.py
@@ -662,7 +662,7 @@ static PyTypeObject AST_type = {
     0,                       /* tp_print */
     0,                       /* tp_getattr */
     0,                       /* tp_setattr */
-    0,                       /* tp_compare */
+    0,                       /* tp_reserved */
     0,                       /* tp_repr */
     0,                       /* tp_as_number */
     0,                       /* tp_as_sequence */

Python/Python-ast.c
@@ -479,7 +479,7 @@ static PyTypeObject AST_type = {
     0,                       /* tp_print */
     0,                       /* tp_getattr */
     0,                       /* tp_setattr */
-    0,                       /* tp_compare */
+    0,                       /* tp_reserved */
     0,                       /* tp_repr */
     0,                       /* tp_as_number */
     0,                       /* tp_as_sequence */

Lib/tkinter/test/support.py
@@ -15,6 +15,14 @@ def get_tk_root():
 
     return root
 
+def root_deiconify():
+    root = get_tk_root()
+    root.deiconify()
+
+def root_withdraw():
+    root = get_tk_root()
+    root.withdraw()
+
 
 def simulate_mouse_click(widget, x, y):
     """Generate proper events to click at the x, y position (tries to act

Lib/tkinter/test/test_ttk/test_extensions.py
@@ -10,6 +10,13 @@ requires('gui')
 
 class LabeledScaleTest(unittest.TestCase):
 
+    def setUp(self):
+        support.root_deiconify()
+
+    def tearDown(self):
+        support.root_withdraw()
+
+
     def test_widget_destroy(self):
         # automatically created variable
         x = ttk.LabeledScale()
@@ -175,12 +182,12 @@ class LabeledScaleTest(unittest.TestCase):
 class OptionMenuTest(unittest.TestCase):
 
     def setUp(self):
-        self.root = support.get_tk_root()
-        self.textvar = tkinter.StringVar(self.root)
+        support.root_deiconify()
+        self.textvar = tkinter.StringVar()
 
     def tearDown(self):
         del self.textvar
-        self.root.destroy()
+        support.root_withdraw()
 
 
     def test_widget_destroy(self):

Lib/tkinter/test/test_ttk/test_style.py
@@ -10,15 +10,7 @@ requires('gui')
 class StyleTest(unittest.TestCase):
 
     def setUp(self):
-        self.root = support.get_tk_root()
-        self.style = ttk.Style(self.root)
-
-    def tearDown(self):
-        # As tests have shown, these tests are likely to deliver
-        # <<ThemeChanged>> events after the root is destroyed, so
-        # lets let them happen now.
-        self.root.update_idletasks()
-        self.root.destroy()
+        self.style = ttk.Style()
 
 
     def test_configure(self):

Lib/tkinter/test/test_ttk/test_widgets.py
@@ -12,12 +12,14 @@ class WidgetTest(unittest.TestCase):
     """Tests methods available in every ttk widget."""
 
     def setUp(self):
+        support.root_deiconify()
         self.widget = ttk.Button()
         self.widget.pack()
         self.widget.wait_visibility()
 
     def tearDown(self):
         self.widget.destroy()
+        support.root_withdraw()
 
 
     def test_identify(self):
@@ -107,10 +109,12 @@ class CheckbuttonTest(unittest.TestCase):
 class ComboboxTest(unittest.TestCase):
 
     def setUp(self):
+        support.root_deiconify()
         self.combo = ttk.Combobox()
 
     def tearDown(self):
         self.combo.destroy()
+        support.root_withdraw()
 
     def _show_drop_down_listbox(self):
         width = self.combo.winfo_width()
@@ -195,10 +199,12 @@ class ComboboxTest(unittest.TestCase):
 class EntryTest(unittest.TestCase):
 
     def setUp(self):
+        support.root_deiconify()
         self.entry = ttk.Entry()
 
     def tearDown(self):
         self.entry.destroy()
+        support.root_withdraw()
 
 
     def test_bbox(self):
@@ -297,10 +303,12 @@ class EntryTest(unittest.TestCase):
 class PanedwindowTest(unittest.TestCase):
 
     def setUp(self):
+        support.root_deiconify()
         self.paned = ttk.Panedwindow()
 
     def tearDown(self):
         self.paned.destroy()
+        support.root_withdraw()
 
 
     def test_add(self):
@@ -445,12 +453,14 @@ class RadiobuttonTest(unittest.TestCase):
 class ScaleTest(unittest.TestCase):
 
     def setUp(self):
+        support.root_deiconify()
         self.scale = ttk.Scale()
         self.scale.pack()
         self.scale.update()
 
     def tearDown(self):
         self.scale.destroy()
+        support.root_withdraw()
 
 
     def test_custom_event(self):
@@ -519,6 +529,7 @@ class ScaleTest(unittest.TestCase):
 class NotebookTest(unittest.TestCase):
 
     def setUp(self):
+        support.root_deiconify()
         self.nb = ttk.Notebook()
         self.child1 = ttk.Label()
         self.child2 = ttk.Label()
@@ -529,6 +540,7 @@ class NotebookTest(unittest.TestCase):
         self.child1.destroy()
         self.child2.destroy()
         self.nb.destroy()
+        support.root_withdraw()
 
 
     def test_tab_identifiers(self):
@@ -708,13 +720,12 @@ class NotebookTest(unittest.TestCase):
 class TreeviewTest(unittest.TestCase):
 
     def setUp(self):
-        self.root = support.get_tk_root()
-        self.tv = ttk.Treeview(self.root)
+        support.root_deiconify()
+        self.tv = ttk.Treeview()
 
     def tearDown(self):
         self.tv.destroy()
-        self.root.update_idletasks()
-        self.root.destroy()
+        support.root_withdraw()
 
 
     def test_bbox(self):