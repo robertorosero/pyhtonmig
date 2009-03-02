fedef47a4e433c0cc7fb90cb1c783f4978114988
antoine.pitrou <antoine.pitrou@mycompany.com>
2009-03-02 23:11:55 +0000

    Looks like this is necessary in order to build cleanly under Windows (someone correct this if it's wrong, I'm no Windows user)
    git-svn-id: http://svn.python.org/projects/python/branches/io-c@70112 6015fed2-1504-0410-9fe1-9d1591cc4771

--- PC/config.c
+++ PC/config.c
@@ -59,7 +59,6 @@ extern PyObject* PyInit__subprocess(void);
 extern PyObject* PyInit__lsprof(void);
 extern PyObject* PyInit__ast(void);
 extern PyObject* PyInit__io(void);
-extern PyObject* PyInit__stringio(void);
 extern PyObject* PyInit__pickle(void);
 extern PyObject* PyInit_atexit(void);
 extern PyObject* _PyWarnings_Init(void);
@@ -150,7 +149,6 @@ struct _inittab _PyImport_Inittab[] = {
         {"_warnings", _PyWarnings_Init},
 
         {"_io", PyInit__io},
-        {"_stringio", PyInit__stringio},
         {"_pickle", PyInit__pickle},
         {"atexit", PyInit_atexit},
 
