00ae457e3e85f6d236e987d7f6fb1adfe698bb17
guilherme.polo <guilherme.polo@mycompany.com>
2009-06-01 20:36:32 +0000

    A very small set of tests for Tkinter.Toplevel (there isn't much to test here actually).
    git-svn-id: http://svn.python.org/projects/python/branches/tk_and_idle_maintenance@73113 6015fed2-1504-0410-9fe1-9d1591cc4771

---
+++ Lib/lib-tk/test/test_tkinter/test_toplevel.py
@@ -0,0 +1,27 @@
+import unittest
+import Tkinter
+from test.test_support import requires, run_unittest
+from ttk import setup_master
+
+requires('gui')
+
+class ToplevelTest(unittest.TestCase):
+
+    def setUp(self):
+        self.root = setup_master()
+
+
+    def test_initialization(self):
+        self.root.title('hi')
+        tl = Tkinter.Toplevel(self.root, **{'class_': 'test'})
+        self.assertEqual(tl['class'], 'test')
+        self.assertEqual(tl.title(), 'hi')
+        tl.destroy()
+
+        self.assertRaises(Tkinter.TclError, Tkinter.Toplevel, self.root, a='b')
+
+
+tests_gui = (ToplevelTest, )
+
+if __name__ == "__main__":
+    run_unittest(*tests_gui)
