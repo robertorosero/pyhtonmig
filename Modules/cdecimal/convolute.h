89c4a3fd1897ad5414da306db6614d3c48707dbf
stefan.krah <stefan.krah@mycompany.com>
2010-01-21 15:06:34 +0000

    Change size_t to mpd_size_t.
    git-svn-id: http://svn.python.org/projects/python/branches/py3k-cdecimal@77629 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Modules/cdecimal/convolute.h
+++ Modules/cdecimal/convolute.h
@@ -13,8 +13,8 @@
 
 #define SIX_STEP_THRESHOLD 4096
 
-int fnt_convolute(mpd_uint_t *c1, mpd_uint_t *c2, size_t n, int modnum);
-int fnt_autoconvolute(mpd_uint_t *c1, size_t n, int modnum);
+int fnt_convolute(mpd_uint_t *c1, mpd_uint_t *c2, mpd_size_t n, int modnum);
+int fnt_autoconvolute(mpd_uint_t *c1, mpd_size_t n, int modnum);
 
 
 #endif
