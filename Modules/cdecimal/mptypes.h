d6bff103e86fcc2bfd004042212a9bf004cc94ea
stefan.krah <stefan.krah@mycompany.com>
2010-01-21 15:41:10 +0000

    Fix OS specific conversion specifiers.
    git-svn-id: http://svn.python.org/projects/python/branches/py3k-cdecimal@77645 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Modules/cdecimal/mptypes.h
+++ Modules/cdecimal/mptypes.h
@@ -10,10 +10,10 @@
 
 #if defined(CONFIG_64)
   #if defined(_MSC_VER)
-    #define PRI_size_t "llu"
+    #define PRI_mpd_size_t "llu"
     #define PRI_mpd_ssize_t "lld"
   #else
-    #define PRI_size_t "lu"
+    #define PRI_mpd_size_t "lu"
     #define PRI_mpd_ssize_t "ld"
   #endif
   #define PRI_time_t "ld"
@@ -21,20 +21,30 @@
   #define ONE_UM 1ULL
 #elif defined(CONFIG_32)
   #if defined (__OpenBSD__)
-    #define PRI_size_t "lu"
-    #define PRI_mpd_ssize_t "ld"
+    #define PRI_mpd_size_t "u"
+    #define PRI_mpd_ssize_t "d"
     #define PRI_time_t "d"
+  #elif defined(__FreeBSD__)
+    #define PRI_mpd_size_t "u"
+    #define PRI_mpd_ssize_t "d"
+    #if defined(__x86_64__)
+      #define PRI_time_t "ld"
+    #else
+      #define PRI_time_t "d"
+    #endif
   #else
-    #define PRI_size_t "u"
+    #define PRI_mpd_size_t "u"
     #define PRI_mpd_ssize_t "d"
     #define PRI_time_t "ld"
   #endif
-  #define CONV_mpd_ssize_t "l"
+  #if MPD_SSIZE_MAX != INT_MAX
+    #error "define CONV_mpd_ssize_t"
+  #endif
+  #define CONV_mpd_ssize_t "i"
   #define ONE_UM 1UL
 #else
   #error "define CONFIG_64 or CONFIG_32"
 #endif
-
 
 #endif
 
