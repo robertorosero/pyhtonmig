d79da38ed440ffad7466a4f4734b6561d7f12fe4
victor.stinner <victor.stinner@mycompany.com>
2010-07-31 11:01:07 +0000

    Merge with py3k
    svn merge -r 83295:HEAD svn+ssh://pythondev@svn.python.org/python/branches/py3k
    
    
    git-svn-id: http://svn.python.org/projects/python/branches/import_unicode@83340 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/test/test_urllib.py
+++ Lib/test/test_urllib.py
@@ -531,6 +531,7 @@ class QuotingTests(unittest.TestCase):
         self.assertEqual(expect, result,
                          "using quote_plus(): %r != %r" % (expect, result))
 
+
 class UnquotingTests(unittest.TestCase):
     """Tests for unquote() and unquote_plus()
 
@@ -558,7 +559,9 @@ class UnquotingTests(unittest.TestCase):
         self.assertEqual(result.count('%'), 1,
                          "using unquote(): not all characters escaped: "
                          "%s" % result)
-        self.assertRaises(TypeError, urllib.parse.unquote, None)
+        self.assertRaises((TypeError, AttributeError), urllib.parse.unquote, None)
+        self.assertRaises((TypeError, AttributeError), urllib.parse.unquote, ())
+        self.assertRaises((TypeError, AttributeError), urllib.parse.unquote, b'')
 
     def test_unquoting_badpercent(self):
         # Test unquoting on bad percent-escapes
@@ -593,8 +596,8 @@ class UnquotingTests(unittest.TestCase):
         result = urllib.parse.unquote_to_bytes(given)
         self.assertEqual(expect, result, "using unquote_to_bytes(): %r != %r"
                          % (expect, result))
-
-        self.assertRaises(TypeError, urllib.parse.unquote_to_bytes, None)
+        self.assertRaises((TypeError, AttributeError), urllib.parse.unquote_to_bytes, None)
+        self.assertRaises((TypeError, AttributeError), urllib.parse.unquote_to_bytes, ())
 
     def test_unquoting_mixed_case(self):
         # Test unquoting on mixed-case hex digits in the percent-escapes
