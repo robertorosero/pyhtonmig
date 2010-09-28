b7b890a424bd310f249d24df07f7f00854b109f6
hirokazu.yamamoto <hirokazu.yamamoto@mycompany.com>
2010-09-28 12:22:07 +0000

    Try to fix "another process is using files" error in *clean* faze.
    git-svn-id: http://svn.python.org/projects/python/branches/release27-maint-ttk-debug-on-xp5@85057 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Tools/buildbot/clean.bat
+++ Tools/buildbot/clean.bat
@@ -1,5 +1,6 @@
 @rem Used by the buildbot "clean" step.
 call "%VS90COMNTOOLS%vsvars32.bat"
+vcbuild /useenv PCbuild\kill_python.vcproj "Debug|Win32" && PCbuild\kill_python_d.exe
 @echo Deleting .pyc/.pyo files ...
 del /s Lib\*.pyc Lib\*.pyo
 cd PCbuild
