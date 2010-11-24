1cfc0da990b2a8d2b0e35510d269aa6894115d1c
david.malcolm <david.malcolm@mycompany.com>
2010-11-24 23:27:14 +0000

    Add the JUMP_IF_SPECIALIZABLE opcode to the peepholer (fixing the jump target fixer-upper, and other places)
    git-svn-id: http://svn.python.org/projects/python/branches/dmalcolm-ast-optimization-branch@86740 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Python/peephole.c
+++ Python/peephole.c
@@ -14,10 +14,10 @@
 #define GETARG(arr, i) ((int)((arr[i+2]<<8) + arr[i+1]))
 #define UNCONDITIONAL_JUMP(op)  (op==JUMP_ABSOLUTE || op==JUMP_FORWARD)
 #define CONDITIONAL_JUMP(op) (op==POP_JUMP_IF_FALSE || op==POP_JUMP_IF_TRUE \
-    || op==JUMP_IF_FALSE_OR_POP || op==JUMP_IF_TRUE_OR_POP)
+    || op==JUMP_IF_FALSE_OR_POP || op==JUMP_IF_TRUE_OR_POP || op==JUMP_IF_SPECIALIZABLE)
 #define ABSOLUTE_JUMP(op) (op==JUMP_ABSOLUTE || op==CONTINUE_LOOP \
     || op==POP_JUMP_IF_FALSE || op==POP_JUMP_IF_TRUE \
-    || op==JUMP_IF_FALSE_OR_POP || op==JUMP_IF_TRUE_OR_POP)
+    || op==JUMP_IF_FALSE_OR_POP || op==JUMP_IF_TRUE_OR_POP || op==JUMP_IF_SPECIALIZABLE)
 #define JUMPS_ON_TRUE(op) (op==POP_JUMP_IF_TRUE || op==JUMP_IF_TRUE_OR_POP)
 #define GETJUMPTGT(arr, i) (GETARG(arr,i) + (ABSOLUTE_JUMP(arr[i]) ? 0 : i+3))
 #define SETARG(arr, i, val) arr[i+2] = val>>8; arr[i+1] = val & 255
@@ -661,6 +661,7 @@ PyCode_Optimize(PyObject *code, PyObject* consts, PyObject *names,
             case POP_JUMP_IF_TRUE:
             case JUMP_IF_FALSE_OR_POP:
             case JUMP_IF_TRUE_OR_POP:
+            case JUMP_IF_SPECIALIZABLE:
                 j = addrmap[GETARG(codestr, i)];
                 SETARG(codestr, i, j);
                 break;
