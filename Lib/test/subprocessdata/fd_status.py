0dae64f56b75d7bf490d5cea885c5951c480eb17
antoine.pitrou <antoine.pitrou@mycompany.com>
2011-01-29 10:09:15 +0000

    Try to ignore Solaris door files in fd_status.py
    git-svn-id: http://svn.python.org/projects/python/branches/test_subprocess_10826@88224 6015fed2-1504-0410-9fe1-9d1591cc4771

--- Lib/test/subprocessdata/fd_status.py
+++ Lib/test/subprocessdata/fd_status.py
@@ -3,32 +3,22 @@ file descriptors on stdout."""
 
 import errno
 import os
-import fcntl
-import subprocess
-import sys
 
 try:
     _MAXFD = os.sysconf("SC_OPEN_MAX")
 except:
     _MAXFD = 256
 
-def isopen(fd):
-    """Return True if the fd is open, and False otherwise"""
-    try:
-        fcntl.fcntl(fd, fcntl.F_GETFD, 0)
-    except IOError as e:
-        if e.errno == errno.EBADF:
-            return False
-        raise
-    return True
-
 if __name__ == "__main__":
-    fds = [fd for fd in range(0, _MAXFD) if isopen(fd)]
+    fds = []
+    for fd in range(0, _MAXFD):
+        try:
+            st = os.fstat(fd)
+        except OSError as e:
+            if e.errno == errno.EBADF:
+                continue
+            raise
+        # Ignore Solaris door files
+        if st.st_mode & 0xF000 != 0xd000:
+            fds.append(fd)
     print(','.join(map(str, fds)))
-    if '--debug' in sys.argv[1:]:
-        for fd in fds:
-            print(fd, fcntl.fcntl(fd, fcntl.F_GETFL), os.fstat(fd))
-        sys.stdout.flush()
-        subprocess.call("ls -la /proc/{}/fd/".format(os.getpid()), shell=True)
-        if 5 in fds:
-            subprocess.call("stat /proc/{}/fd/{}".format(os.getpid(), 5), shell=True)
